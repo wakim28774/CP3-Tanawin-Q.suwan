4c876aaece071526435828c8e693172f93e24fda
wakim28774 <148669408+wakim28774@users.noreply.github.com>
2023-10-29 16:30:28 +0700

    Excerise4

---
+++ Excerise4_Tanawin_Q.py
@@ -0,0 +1,9 @@
+Found_Eng = 90
+Gen_Bu = 80
+Com_System = 70
+Com_Pro = 60
+print("--- Your Score ---")
+print("Foundation English               :", Found_Eng)
+print("General Business                 :", Gen_Bu)
+print("Introduction to Computer Systems :", Com_System)
+print("Computer Programming             :", Com_Pro)
