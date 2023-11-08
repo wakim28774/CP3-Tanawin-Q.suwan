bafd0c785a102e7d035dc011cdc6e738da7ca46b
wakim28774 <148669408+wakim28774@users.noreply.github.com>
2023-11-08 18:45:59 +0700

    Create Lecture50_Tanawin_Q.py

---
+++ Lecture50_Tanawin_Q.py
@@ -0,0 +1,7 @@
+def Math(x,y):
+    print(str(x),"+",str(y),"=",x+y)
+    print(str(x),"-",str(y),"=",x-y)
+    print(str(x),"*",str(y),"=",x*y)
+    print(str(x),"/",str(y),"=",x/y)
+
+Math(x = int(input("Enter Number :")),y = int(input("Enter Number :")))
